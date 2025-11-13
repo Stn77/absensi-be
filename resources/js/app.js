e700cd1153b412264d6f4a4c3a8cf90235f34d72
Stn77 <fudinyusuf20@gmail.com>
2025-11-13 12:02:59 +0700

    feat: Add custom Bootstrap styles and initialize tooltips
    - Imported custom SCSS file in app.js and initialized Bootstrap tooltips on DOMContentLoaded.
    - Created app.scss with custom Bootstrap variables and styles for headers and cards.
    - Updated vite.config.js to enhance server configuration for development.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import '../sass/app.scss';
 import './bootstrap';
+
+// Import Bootstrap JavaScript
+import * as bootstrap from 'bootstrap';
+
+// Contoh: Inisialisasi semua tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
