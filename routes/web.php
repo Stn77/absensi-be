c711dd6a91561afab169449042f0b6140c30f108
Stn77 <fudinyusuf20@gmail.com>
2025-11-13 12:01:59 +0700

    fix: update default cache store to 'file' and adjust PostgreSQL connection settings

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\Monolith\Auth\WebAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->middleware('guest')->controller(WebAuthController::class)->group(function () {
+    Route::get('login', 'showLoginForm')->name('login.page');
+    Route::get('login/s', 'loginSubmit')->name('login.submit');
+
+    Route::get('register', 'showRegistrationForm')->name('register.page');
+    Route::get('register/s', 'registrationSubmit')->name('register.submit');
+});
+
+Route::prefix('dashboard')->middleware('auth')->group(function () {
+
 });
