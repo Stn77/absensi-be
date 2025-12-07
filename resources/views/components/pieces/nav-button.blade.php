6b77d00cda115bfde939637c3f536e348934bd28
Stn77 <fudinyusuf20@gmail.com>
2025-12-07 21:02:27 +0700

    feat and reafctor component

--- resources/views/components/pieces/nav-button.blade.php
+++ resources/views/components/pieces/nav-button.blade.php
@@ -1,6 +1,8 @@
+@hasanyrole($role)
 <li class="menu-item">
     <a href="{{route($link)}}" class="menu-link {{Route::is($link) ? 'active' : ''}}">
         <span class="menu-icon"><i class="fas fa-{{$icon}}"></i></span>
         <span class="menu-text">{{$name}}</span>
     </a>
 </li>
+@endhasanyrole
--- resources/views/components/pieces/sdbline.blade.php
+++ resources/views/components/pieces/sdbline.blade.php
@@ -1,3 +1,3 @@
-<div class="border mx-auto" style="width: 85%; opacity: 0.5;">
+<div class="border mx-auto my-2" style="width: 85%; opacity: 0.5;">
     <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
 </div>
