da75a2d16a7112b871f48aa7ed34aee0703c7658
Stn77 <fudinyusuf20@gmail.com>
2026-01-07 19:37:13 +0700

    feat: create profile view layout

---
+++ resources/views/aio/profile.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.app title="Profile {{Auth::user()->name}}" pageTitleName="Profile {{Auth::user()->name}}">
+    @push('style')
+
+    @endpush
+
+    @push('script')
+    <script>
+        
+    </script>
+    @endpush
+</x-layouts.app>
