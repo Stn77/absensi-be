1c4b4073b919b47531f9372ba38a7ecb44c38402
bulbul1945 <ervashohibulkahfi@gmail.com>
2026-01-29 07:26:16 +0700

    menambahkan fitur get data dan menampilkannya

--- resources/views/izin/index.blade.php
+++ resources/views/izin/index.blade.php
@@ -2,65 +2,80 @@
     <div class="d-flex flex-column">
         <div class="mb-4 d-flex">
             <div class="flex-row filter d-flex form-group">
-                @hasanyrole('admin|teacher')
+
+                @hasanyrole('admin|guru')
                 <div class="jurusan-f me-2 form-group">
-                    <select class="form-select form-select-sm me-2" name="kelas" id="kelasFilter" aria-placeholder="konz">
-                        <option value="">kelas</option>
+                    <select class="form-select form-select-sm me-2" name="kelas" id="kelasFilter">
+                        <option value="">Semua Kelas</option>
                         <option value="1">X</option>
                         <option value="2">XI</option>
                         <option value="3">XII</option>
                     </select>
                 </div>
+
                 <div class="kelas-f me-2 form-group">
                     <select class="form-select form-select-sm me-2" name="jurusan" id="jurusanFilter">
-                        <option value="">Jurusan</option>
+                        <option value="">Semua Jurusan</option>
                         <option value="1">MP</option>
                         <option value="2">AK</option>
                         <option value="3">BD</option>
                         <option value="4">TSM</option>
                         <option value="5">DKV</option>
                         <option value="6">PPLG</option>
-                        <option value="7">tkkr</option>
+                        <option value="7">TKKR</option>
                     </select>
                 </div>
                 @endhasanyrole
 
-                @hasanyrole('admin')
-
-                @endhasanyrole
             </div>
         </div>
+
         <div class="table-responsive">
-            <table class="table mt-2 table-striped table-bordered" id="izinSiswa">
-                <thead class="mt-2 text-light table-dark" style="background-color: rgb(32, 32, 32);"></thead>
+            <table class="table mt-2 table-striped table-bordered" id="izinSiswa" style="width:100%">
+                <thead class="table-dark"></thead>
             </table>
         </div>
     </div>
 
     @push('script')
-        @hasanyrole('siswa')
-        <script type="module">
-            $(document).ready(() => {
-                let tabelIzin = $('#izinSiswa').Datatable({
+    <script>
+        $(document).ready(function () {
+
+            let tabelIzin = $('#izinSiswa').DataTable({
                 processing: true,
-                pageLength: 50,
                 serverSide: true,
-                autoFill: false,
+                pageLength: 25,
                 ajax: {
-                    url: ,
+                    url: "{{ route('siswa.izin.list.fetch') }}",
+                    data: function(d) {
+                        d.kelas = $('#kelasFilter').val();
+                        d.jurusan = $('#jurusanFilter').val();
+                    }
                 },
                 columns: [
-                    {
-                        data: 'DT_RowIndex',
-                        title: 'No',
-                        orderable: false,
-                    },
+                    { data: 'DT_RowIndex', title: 'No', orderable: false },
 
-                    { data: 'action', title: 'Ket', orderable: false, searchable: false },
+                    { data: 'nama', title: 'Nama' },
+
+                    { data: 'kelas', title: 'Kelas' },
+
+                    { data: 'jenis', title: 'Jenis Izin' },
+
+                    { data: 'keperluan', title: 'Keperluan' },
+
+                    { data: 'from_date', title: 'Mulai' },
+
+                    { data: 'until_date', title: 'Sampai' },
+
+                    { data: 'action', title: 'Aksi', orderable: false }
                 ]
-                })
-            })
-        </script>
-        @endhasanyrole
+            });
+
+            $('#kelasFilter, #jurusanFilter').on('change', function () {
+                tabelIzin.ajax.reload();
+            });
+
+        });
+    </script>
     @endpush
 </x-layouts.app>
