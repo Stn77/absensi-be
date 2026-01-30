ee43ee8d08ba8a6421970a59a109b28f998b4153
Stn77 <fudinyusuf20@gmail.com>
2026-01-30 08:28:08 +0700

    hehe

---
+++ resources/views/data/siswa-create.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.app title="Export Absensi Siswa" pageTitleName="Export Absensi Siswa">
+    @push('style')
+
+    @endpush
+    <div class="container-fluid border">
+        
+    </div>
+    @push('script')
+
+    @endpush
+</x-layouts.app>
